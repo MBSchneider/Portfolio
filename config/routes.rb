b4cbeb57e85834b4d15b6237525286d1c7584263
mattschneider2002 <matts3@u.washington.edu>
2013-10-10 16:05:44 -0700

    Update routes, home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Portfolio::Application.routes.draw do
+  get "home/index"
+
   resources :posts
 
 
@@ -51,7 +53,7 @@ Portfolio::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root 'home#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
