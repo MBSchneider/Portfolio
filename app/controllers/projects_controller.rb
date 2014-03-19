7a0c1563854fc0b2cb015ff95dd7648713468cac
MBSchneider <matts3@u.washington.edu>
2014-03-18 23:40:53 -0700

    Updates prioritization of projects, test for this

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -14,13 +14,11 @@ class ProjectsController < ApplicationController
     @project = Project.new(params[:project])
     respond_to do |format|
       if @project.save
-        re_prioritize(@project)
-
+        re_prioritize(@project, Project.all.length + 1)
         format.html do
           redirect_to @project,
                       notice: 'Project was successfully created.'
         end
-
         format.json do
           render json: @project,
                  status: :created,
@@ -60,8 +58,9 @@ class ProjectsController < ApplicationController
 
   def update
     @project = Project.find(params[:id])
+    old_priority = @project.priority
     if @project.update_attributes(params[:project])
-      re_prioritize(@project)
+      re_prioritize(@project, old_priority)
       redirect_to @project, notice: 'Project was successfully updated.'
     else
       render action: 'edit'
@@ -74,11 +73,20 @@ class ProjectsController < ApplicationController
     redirect_to projects_path
   end
 
-  def re_prioritize(project)
-    Project.order(:priority)[project.priority..-1].each do |pr|
-      unless pr == @project || pr.priority.nil?
-        pr.priority += 1
-        pr.save
+  def re_prioritize(project, old_priority)
+    if old_priority > project.priority
+      Project.order(:priority)[project.priority - 1..old_priority - 1].each do |pr|
+        unless pr == project || pr.priority.nil?
+          pr.priority += 1
+          pr.save
+        end
+      end
+    elsif old_priority < project.priority
+      Project.order(:priority)[old_priority - 1..project.priority - 1].each do |pr|
+        unless pr == project || pr.priority.nil?
+          pr.priority -= 1
+          pr.save
+        end
       end
     end
   end
--- test/features/projects/project_prioritize_test.rb
+++ test/features/projects/project_prioritize_test.rb
@@ -3,31 +3,31 @@ require 'test_helper'
 feature 'prioritize projects as an editor' do
   scenario 'decrease project priority' do
     editor_sign_in
-
     visit project_path(projects(:mdp))
     click_link 'Edit'
     fill_in 'Priority', with: 3
     click_on 'Update Project'
     visit projects_path
 
-    projects(:bp).priority.must_equal 1
-    projects(:cfb).priority.must_equal 2
-    projects(:mdp).priority.must_equal 3
+    Project.find(projects(:bp)).priority.must_equal 1
+    Project.find(projects(:cfb)).priority.must_equal 2
+    Project.find(projects(:mdp)).priority.must_equal 3
+    Project.find(projects(:dd)).priority.must_equal 4
   end
 end
 
 feature 'prioritize projects as an editor' do
   scenario 'increase project priority' do
     editor_sign_in
-
     visit project_path(projects(:cfb))
     click_link 'Edit'
     fill_in 'Priority', with: 1
     click_on 'Update Project'
     visit projects_path
 
-    projects(:cfb).priority.must_equal 1
-    projects(:mdp).priority.must_equal 2
-    projects(:bp).priority.must_equal 3
+    Project.find(projects(:cfb)).priority.must_equal 1
+    Project.find(projects(:mdp)).priority.must_equal 2
+    Project.find(projects(:bp)).priority.must_equal 3
+    Project.find(projects(:dd)).priority.must_equal 4
   end
 end
